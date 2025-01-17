3ed4e24ad992132c621525b0debd1808cf4677c8
NITISH K <117919051+nitish-k00@users.noreply.github.com>
2025-01-17 17:54:46 +0530

    removed lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,37 @@
-import { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Main from "./components/Main";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Stack from "./components/Stack";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import Description from "./projects/Description";
+import ProjectContainer from "./projects/ProjectContainer";
 
-// Lazy load components
-const NavBar = lazy(() => import("./components/NavBar"));
-const Main = lazy(() => import("./components/Main"));
-const Projects = lazy(() => import("./components/Projects"));
-const Skills = lazy(() => import("./components/Skills"));
-const Stack = lazy(() => import("./components/Stack"));
-const Experience = lazy(() => import("./components/Experience"));
-const Footer = lazy(() => import("./components/Footer"));
-const Description = lazy(() => import("./projects/Description"));
-const ProjectContainer = lazy(() => import("./projects/ProjectContainer"));
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Router>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <NavBar />
-                  <Main />
-                  <Projects />
-                  <Skills />
-                  <Stack />
-                  <Experience />
-                  <Footer />
-                </>
-              }
-            />
-            <Route path="/project/:id" element={<ProjectContainer />} />
-            <Route path="/description/:id" element={<Description />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <NavBar />
+                <Main />
+                <Projects />
+                <Skills />
+                <Stack />
+                <Experience />
+                <Footer />
+              </>
+            }
+          ></Route>
+          <Route path="/project/:id" element={<ProjectContainer />} />
+          <Route path="/description/:id" element={<Description />} />
+        </Routes>
       </Router>
     </div>
   );
