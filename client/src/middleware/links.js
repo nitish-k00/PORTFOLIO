cbad43a400fc6ac375bd333994f60bb37afe5a24
NITISH K <117919051+nitish-k00@users.noreply.github.com>
2025-01-29 00:05:59 +0530

    Update resume url in  links.js

--- client/src/middleware/links.js
+++ client/src/middleware/links.js
@@ -1,6 +1,4 @@
-const Resume =
-  "https://drive.google.com/file/d/1ubTKFtZy3AjBJAgN6zX_eKl6itylQqKT";
-
+const Resume ="https://drive.google.com/file/d/1U2CVYW51NsH-SBxm88DBmEe7Bs9arhHm/view";
 const LindedIn = "https://www.linkedin.com/in/nitish-k-580985280/";
 const GitHub = "https://github.com/nitish-k00";
 
