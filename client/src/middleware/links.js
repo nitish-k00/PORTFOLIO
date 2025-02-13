4499ba4a70ea80ba0f0c59b4b612817d80e61ade
NITISH K <117919051+nitish-k00@users.noreply.github.com>
2025-02-13 22:11:15 +0530

    Update links.js

--- client/src/middleware/links.js
+++ client/src/middleware/links.js
@@ -1,4 +1,4 @@
-const Resume ="https://drive.google.com/file/d/1Y5ix6yyHPYCV9HTQbaSyiqdxS8BOGBVM";
+const Resume ="https://drive.google.com/file/d/1YWG30ZlCIIaOFUioIj7Eatqj6WDxV-bl/view";
 const LindedIn = "https://www.linkedin.com/in/nitish-k-580985280/";
 const GitHub = "https://github.com/nitish-k00";
 
