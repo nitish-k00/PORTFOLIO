292baada4bdd52d40758078321d83f5fc67d6d8f
NITISH K <117919051+nitish-k00@users.noreply.github.com>
2025-02-23 11:51:09 +0530

    Update exp-data.js

--- client/src/middleware/exp-data.js
+++ client/src/middleware/exp-data.js
@@ -1,10 +1,17 @@
 const Experiences = [
   {
     id: "01",
-    title: "E-Commerce Optimization Platform",
-    heading: "Freelancer (Full Stack Developer)",
+    title: "Freelancer (Full Stack Developer)", 
+    heading: "E-Commerce Optimization Platform",
     content:
       "Designed a comprehensive platform to streamline e-commerce operations, including a dynamic admin panel to manage over 500 products and process 400+ orders monthly. Enhanced inventory, categories, and user review oversight. Integrated JWT, Google Login, and OTP for secure data protection. Improved delivery efficiency by 25% and increased success rates by 70% with Razorpay integration and data visualization. Reduced customer inquiries by 30% with live map updates and cut backend response times by 50% using Redis.",
+  },
+  {
+    id: "02",
+    title: "Intern (Front-End Developer)",
+    heading: "Qsectron",
+    content:
+      "Debugged and optimized React.js applications, improving load times by 30% Developed robust, responsive, and accessible UI designs, leading to a 25% increase in user engagement. Integrated REST APIs and collaborated with backend teams, reducing data fetch time by 40%. Used reusable components, cutting development time for new features by 35%. Made minor backend modifications using Node.js & Express.js to support frontend features.",
   },
 ];
 
